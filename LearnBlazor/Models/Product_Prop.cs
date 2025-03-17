ae7f35e7d4c1210b3cdd67490ceed6560d2565e6
Teeradet <tee2490@hotmail.com>
2025-03-17 13:46:41 +0700

    Section 3 - Dropdowns and Data Binding

---
+++ LearnBlazor/Models/Product_Prop.cs
@@ -0,0 +1,10 @@
+﻿namespace LearnBlazor.Models
+{
+    public class Product_Prop
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+    }
+}
